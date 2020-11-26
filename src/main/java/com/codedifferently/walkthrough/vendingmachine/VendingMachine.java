e88de3fafa5dcfcdc9af611b590942e4a3f9f736
Matt Werth <matthewwerth@gmail.com>
2020-11-26 10:35:11 -0500

    Added Method To Check For Product Availability

--- src/main/java/com/codedifferently/walkthrough/vendingmachine/VendingMachine.java
+++ src/main/java/com/codedifferently/walkthrough/vendingmachine/VendingMachine.java
@@ -1,38 +1,70 @@
 package com.codedifferently.walkthrough.vendingmachine;
 
-import com.codedifferently.walkthrough.vendingmachine.inventory.Product;
 import menu.Menu;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class VendingMachine {
     private Scanner scanner;
-    private Map<String, Product> inventory;
+    private Map<String, Integer> inventoryRemaining;
+    private Map<String, Integer> productPrices;
+    private Map<String, Integer> cart;
     private Menu menu;
 
 
-    public VendingMachine() {
+    public VendingMachine() throws IOException {
         this.scanner = new Scanner(System.in);
+        setInventoryRemaining();
+        setProductPrices();
     }
+
+    public void setInventoryRemaining() throws IOException {
+        inventoryRemaining = new HashMap<>();
+        Stream <String> products = Files.lines(Paths.get("/Users/m21/dev/labs/VendingMachineWalkthrough-Java/productCodeNumberOfItems.txt"));
+        products
+        .forEach(productCode -> {
+            inventoryRemaining.put(productCode, 5);
+        });
+        products.close();
+    }
+
+    public void setProductPrices() throws IOException {
+        productPrices = new HashMap<>();
+        Stream <String> products = Files.lines(Paths.get("/Users/m21/dev/labs/VendingMachineWalkthrough-Java/productCodeAndPrice.txt"));
+        products
+                .forEach(productCode -> {
+                    // comma separated values with File.lines??
+                     productPrices.put(productCode, 5);
+                });
+        products.close();
+    }
+
+    public Boolean checkForProduct(String productID) {
+        if(inventoryRemaining.containsKey(productID) && inventoryRemaining.get(productID) > 0) { //if that choice exists and we have the inventory..
+            return true;
+        }
+        return false;
+    }
+
 
     public void start() throws IOException {
         System.out.println("Welcome To VenDifferently! We Have Dairy Free, GF, Nut Free & Vegan Snacks!");
 
         boolean flag = true;
 
-        ArrayList<String> options = new ArrayList<>();
+        ArrayList<String> initialOptions = new ArrayList<>();
+        initialOptions.add("(1) Display Vending Items");
+        initialOptions.add("(2) Purchase");
+        initialOptions.add("(3) Exit");
 
-        options.add("(1) Display Vending Items");
-        options.add("(2) Purchase");
-        options.add("(3) Exit");
-
-        menu = new Menu(options);
+        menu = new Menu(initialOptions);
 
         while(flag) {
 
@@ -44,9 +76,29 @@ public class VendingMachine {
 
             switch(input) {
                 case "1" :
+                    ArrayList<String> listOfProducts = new ArrayList<>();
+                    Stream <String> products = Files.lines(Paths.get("/Users/m21/dev/labs/VendingMachineWalkthrough-Java/productCodeAndPrice.txt"));
+                    products
+                            .forEach(product -> {
+                                listOfProducts.add(product);
+                                System.out.println(product);
+                            });
+                    products.close();
+                    // Instead of creating a new Menu object we are just SOUT each product as we create it.. This may need to be refactored.
+                    System.out.println("Choose A Product By Typing ID Number");
 
-                    System.out.println("Choose A Product"); // Or Type In A Category. Choose: Candy, Chips, Drinks, Gum or 0 For Our Allergy Menu.");
+                    String usersProductChoice = scanner.next(); //get the users choice
+                    Boolean productAvailable = checkForProduct(usersProductChoice); //see if the product is available and if so add to cart...
 
+                    if(productAvailable) { //if it is
+                        cart.put(usersProductChoice, 1);
+                        System.out.println(usersProductChoice + " Has Been Added To Your Cart!");
+                    }
+                    else {
+                        listOfProducts.remove(usersProductChoice);
+                        System.out.println("Sorry, That Product Is No Longer Available!");
+                    }
+                    break;
 
                 case "2" :
                     System.out.println("Hey");
@@ -54,11 +106,15 @@ public class VendingMachine {
 
                 case "3" :
                     flag = false;
-                    System.out.println("Bye Bye");
+                    System.out.println("Bye Bye Friend.");
 
                 default:
                     System.out.println("Try Again");
             }
+            // TODO: 11/26/20
+            // if user enters a correct id#
+            // check for available funds?
+            // fetch that product from our inventory HashMap.
         }
         scanner.close();
     }
@@ -70,11 +126,10 @@ public class VendingMachine {
 }
 
 
-//                    Stream <String> names = Files.lines(Paths.get("/Users/m21/dev/labs/VendingMachineWalkthrough-Java/allProducts.txt"));
-//    names
-//        .forEach(name -> {
-//                    name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
-//                    System.out.println(name);
+//    Stream <String> products = Files.lines(Paths.get("/Users/m21/dev/labs/VendingMachineWalkthrough-Java/allProducts.txt"));
+//    products
+//        .forEach(product -> {
+//                    System.out.println(product);
 //                });
-//    names.close();
-//}
+//    products.close();
+//
--- src/test/java/com/codedifferently/walkthrough/vendingmachine/VendingMachineTest.java
+++ src/test/java/com/codedifferently/walkthrough/vendingmachine/VendingMachineTest.java
@@ -2,10 +2,12 @@ package com.codedifferently.walkthrough.vendingmachine;
 
 import org.apache.log4j.Logger;
 
+import java.io.IOException;
+
 public class VendingMachineTest {
     private final static Logger logger = Logger.getLogger(VendingMachineTest.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         logger.info("Start");
 
         VendingMachine vend = new VendingMachine();
